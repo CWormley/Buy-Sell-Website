2d07eab257b7d484dfee8ec12236b107ef083386
Claudia Wormley <119722180+CWormley@users.noreply.github.com>
2025-02-18 16:11:35 -0800

    add: about.html and index.html, del: linked webpages with server

--- application/src/WebServer.java
+++ application/src/WebServer.java
@@ -2,32 +2,57 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
-import java.net.InetSocketAddress;
 import java.io.OutputStream;
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.BufferedInputStream;
+import java.net.InetSocketAddress;
 
 public class WebServer {
     public static void main(String[] args) throws IOException {
         int port = 8080;
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 
-        //url path and its handler
-        server.createContext("/", new HttpHandler() {
-            @Override
-            public void handle(HttpExchange exchange) throws IOException {
-                String response = "Hello team 2 for CSC 648 project!";
-                exchange.sendResponseHeaders(200, response.length());
+        // Serve Home Page
+        server.createContext("/", new FileHandler("index.html"));
 
+        // Serve About Page
+        server.createContext("/about", new FileHandler("about.html"));
+
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Web server started at port " + port);
+    }
+
+    static class FileHandler implements HttpHandler {
+        private String filename;
+
+        public FileHandler(String filename) {
+            this.filename = filename;
+        }
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            File file = new File(filename);
+            if (!file.exists()) {
+                String response = "404 Not Found";
+                exchange.sendResponseHeaders(404, response.length());
                 OutputStream os = exchange.getResponseBody();
                 os.write(response.getBytes());
                 os.close();
+                return;
             }
-        });
 
-        //starts server
-        server.setExecutor(null);
-        server.start();
+            byte[] fileBytes = new byte[(int) file.length()];
+            try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
+                bis.read(fileBytes);
+            }
 
-        System.out.println("Web server started at port " + port);
+            exchange.getResponseHeaders().set("Content-Type", "text/html");
+            exchange.sendResponseHeaders(200, fileBytes.length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(fileBytes);
+            os.close();
+        }
     }
 }
