0586e1e5a868218ddd2cac191a83577b43105d25
Daniel Cervantes <dcervantes2@sfsu.edu>
2025-04-03 15:24:37 -0700

    made changes to the database

--- application/src/backend/index.js
+++ application/src/backend/index.js
@@ -1,26 +1,9 @@
 const express = require('express');
-const path = require('path');
-const mysql = require('mysql2');
 
 const app = express();
 const port = 3000;
 
-// MySQL connection
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'yourpassword',
-  database: 'yourdatabase'
-});
 
-// Test database connection
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database: ', err.stack);
-    return;
-  }
-  console.log('Connected to the database');
-});
 
 // Middleware
 app.use(cors());
