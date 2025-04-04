7903327e77ea7a3b2d6b5dab2836bc4ac2148b5a
Daniel Cervantes <dcervantes2@sfsu.edu>
2025-04-03 22:46:35 -0700

    db password

--- application/src/backend/db.js
+++ application/src/backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'database-team02.c87y00ue4gr3.us-east-1.rds.amazonaws.com',
     user: 'team02csc648',
-    password: 'fo9EQ90DHgp590PRD5Lf!',
+    password: 'fo9EQ90DHgp590PRD5Lf',
     database: 'CSC_Test',
     waitForConnections: true,
     connectionLimit: 10,
