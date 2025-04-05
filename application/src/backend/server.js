37f0837f1cb942508c88bd163141bbb2cec344f7
Daniel Cervantes <dcervantes2@sfsu.edu>
2025-04-04 18:08:51 -0700

    updated server for front end

--- application/src/backend/server.js
+++ application/src/backend/server.js
@@ -38,7 +38,8 @@ app.get('/test-db', async (req, res) => {
   });
 
 // Start server
-app.listen(port, () => {
-  console.log(`🚀 Server running at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`🚀 Server running at http://0.0.0.0:${port}`);
 });
+
 
