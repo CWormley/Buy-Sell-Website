488712303efd388d305b0082e8aafd3149b75217
Claudia Wormley <119722180+CWormley@users.noreply.github.com>
2025-04-04 22:14:41 -0700

    wip

--- application/src/backend/server.js
+++ application/src/backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const db = require('./db');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Test DB connection
 db.query('SELECT 1')
